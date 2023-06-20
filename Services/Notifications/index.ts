48e409ec37680cc10c60edd90c6ccb239bcca80b
ThePotatomancer <shimon.karpesh@gmail.com>
2023-06-20 11:18:52 +0300

    Initial setup

---
+++ Services/Notifications/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+
+const port = Number.parseInt(process.env.PORT || "3000");
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Notifications service is listening on port ${port}`);
+})
